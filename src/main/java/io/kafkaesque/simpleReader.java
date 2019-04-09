36639ae3cfa7b0929e7e03d5b8daa60507943a27
Chris Bartholomew <c_bartholomew@yahoo.com>
2019-04-09 12:58:32 -0400

    Updating for secure connection

--- src/main/java/io/kafkaesque/simpleReader.java
+++ src/main/java/io/kafkaesque/simpleReader.java
@@ -6,20 +6,23 @@ import java.util.concurrent.TimeUnit;
 
 public class simpleReader {
 
-    private static final String SERVICE_URL = "pulsar://asiaeast2.gcp.kafkaesque.io:6650";
+    private static final String SERVICE_URL = "pulsar+ssl://useast1.gcp.kafkaesque.io:6651";
 
     public static void main(String[] args) throws IOException {
 
         // Create client object
         PulsarClient client = PulsarClient.builder()
                 .serviceUrl(SERVICE_URL)
+                .authentication(
+                        AuthenticationFactory.token("<INSERT CLIENT TOKEN HERE>")
+                )
                 .build();
 
         // Create a reader on a topic starting at the earliest retained message
         // No subscription is necessary. Depending on retention policy, the
         // earliest message may be days old
         Reader<byte[]> reader = client.newReader()
-                .topic("mytenant2/local-asiaeast2-gcp/test-topic")
+                .topic("chris-kafkaesque-io/local-useast1-gcp/tc1-messages")
                 .startMessageId(MessageId.earliest)
                 .create();
 
